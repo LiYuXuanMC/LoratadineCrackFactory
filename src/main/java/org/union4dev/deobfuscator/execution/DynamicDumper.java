33ae7351981d2f31a267a75e28c42b67d443a068
cubk <cubk@vip.qq.com>
2024-08-27 13:45:09 +0800

    Update monitor

--- src/main/java/org/union4dev/deobfuscator/execution/DynamicDumper.java
+++ src/main/java/org/union4dev/deobfuscator/execution/DynamicDumper.java
@@ -1,9 +1,14 @@
 package org.union4dev.deobfuscator.execution;
 
 import org.tinylog.Logger;
+import org.union4dev.deobfuscator.RunnableMain;
 import org.union4dev.deobfuscator.util.ClassNodeUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.util.jar.JarEntry;
+import java.util.zip.ZipOutputStream;
 
 public class DynamicDumper {
     public static void init() {
@@ -14,5 +19,39 @@ public class DynamicDumper {
         String name = ClassNodeUtil.quickParseBytes(bytes).name;
         Logger.info("Defined Class {}",name);
         return name;
+    }
+
+    public static int a(byte[][] classData) {
+        try {
+            File dump = new File("dumped.jar");
+            if(dump.exists()){
+                Files.delete(dump.toPath());
+            }
+
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("dumped.jar"));
+
+            for(byte[] classBytes : classData) {
+                String name = ClassNodeUtil.quickParseBytes(classBytes).name;
+                Logger.info("Dumped Class {}.",name );
+                if(!name.startsWith("cn/lzq") || name.endsWith("InjectionEndpoint") || name.endsWith("Fucker")){
+                    zipOutputStream.putNextEntry(new JarEntry(name + ".class"));
+                    zipOutputStream.write(classBytes);
+                    zipOutputStream.closeEntry();
+                }
+            }
+
+            zipOutputStream.finish();
+            zipOutputStream.close();
+            Logger.info("Finished dump, start transform");
+            RunnableMain.transform();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static byte[][] a(int size) {
+        return new byte[size][];
     }
 }
